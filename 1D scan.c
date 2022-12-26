d11818eb6093e160ad073ecc72a66da93e0370c3
Vishalp96 <112165749+Vishalp96@users.noreply.github.com>
2022-12-26 11:22:56 +0530

    Add files via upload
    hustel hub selflearning programs

---
+++ 1D scan.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[100],i,n;
+
+    printf("Enter size of array: ");
+    scanf("%d",&n);
+
+    printf("Enter the elements in the array : ");
+    for(i=0; i<n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    printf("\nElements in array are: ");
+    for(i=0; i<n; i++)
+
+    {
+        printf("%d  ", a[i]);
+    }
+
+
+}
